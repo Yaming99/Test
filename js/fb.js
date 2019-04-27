65e7cc0b1cea66b3a68f638b475b7965190b29fa
Anthony Quach <anthony.quach@efrei.net>
2019-04-27 22:22:36 +0200

    Added my own shitty facebook

---
+++ js/fb.js
@@ -0,0 +1,46 @@
+var database = [{
+        username: "Michel",
+        password: "aaa"
+    },
+    {
+        username: "Polnareff",
+        password: "SilverChariot"
+    }
+];
+
+var newsfeed = [{
+        username: "Bob",
+        timeline: "I love traps."
+    },
+    {
+        username: "Jotaro",
+        timeline: "Yare yare daze..."
+    }
+];
+
+function isUserValid(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].username === username && database[i].password === password) {
+            return true;
+        }
+    }
+    alert("Wrong username and password");
+    return false;
+}
+
+function signIn(username, password) {
+    if (isUserValid(username, password)) {
+        console.log(newsfeed);
+    }
+}
+
+function login() {
+    do {
+        var usernamePrompt = prompt("Please enter your username:");
+    	var passwordPrompt = prompt("Please enter your password:");
+    } while (!(isUserValid(usernamePrompt, passwordPrompt)));
+
+    signIn(usernamePrompt, passwordPrompt);
+}
+
+login();
